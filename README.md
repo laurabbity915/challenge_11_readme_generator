124e42438ec6f998c367d1ab59f2234d9aa03d44
laurabbity915 <laura_yuen915@outlook.com>
2023-03-09 22:29:19 +0000

    rewrite index.js let it generate the readme.md

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
-# Readme Generator
-## Create a Readme easily!
-## How to install?
-* download this project and open the vscode and open the terminal and go to the project and type "node index.js"
-
-## Usage
-* After you answer all these questions on terminal, it will create a html file, you can copy the content and create your readme file on a easy way!
-
-## License
+# Description: 
+readme generator
+# Table of Contents: 
+create your readme easily! 
+# installation:
+download this project and open the vscode and open the terminal and go to the project and type "node index.js"
+# Usage:
+after you answer all the question, it will generate a readme.md for you!
+# License:
 MIT
+# Contributing:
+no
+# Test:
+undefined
+# Questions:
+no
 
-## Contributing:
-* none
+walkthough video:
+https://drive.google.com/file/d/1bVYgySMC_eCXXFrisjWI7dBK5oV8T9PV/view?usp=sharing
 
-## Tests:
-* I try to make this Readme file by using this Readme Generator.
-
-## Questions:
-* none
-
-![Kiku](./Screenshot%202023-02-27%20at%2011.47.40%20PM.png)
-
-## Walkthough tutorial video:
-https://drive.google.com/file/d/1qX4qUxBGufIFvnaFH-c5lqQsmBZWixn5/view?usp=sharing
--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-// const fs = require("fs");
-// const path = require('path');
-// const inquirer = require("inquirer");
-// const generateMarkdown = require("./generateMarkdown");
-
-// // array of questions for user
-// const questions = [
-
-// ];
-
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
-
-
-
-
-
-
-
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-const promptUser = () =>
-  inquirer.prompt([
+inquirer.prompt([
     {
       type: 'input',
       name: 'description',
@@ -76,41 +44,145 @@ const promptUser = () =>
       name: 'questions',
       message: 'Questions',
     },
-  ]);
 
-const generateHTML = (answers) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4"> # ${answers.description}</h1>
-    <p> ${answers.toc}</p>
-    <h3> ## How to install?</h3>
-    <p> * ${answers.installation}</p>
-    <h3> ## Usage</h3>
-    <p> * ${answers.usage}</p>
-    <h3> ## License </h3>
-    <p> ${answers.license} </p>
-    <h3> ## Contributing: </h3>
-    <p> * ${answers.contributing} </p>
-    <h3> ## Tests: </h3>
-    <p> * ${answers.tests} </p>
-    <h3> ## Questions: </h3>
-    <p> * ${answers.questions} </p>
-    <p>![Kiku](put your image file name here)</p>
-  </div>
-</div>
-</body>
-</html>`;
+])
 
-promptUser()
-  .then((answers) => writeFileAsync('README.html', generateHTML(answers)))
-  .then(() => console.log('Successfully wrote to README.html'))
-  .catch((err) => console.error(err));
+// Generate README content from user's answers
+  .then((answers) => {
+    const readmeContent = `
+# Description: 
+${answers.description}
+# Table of Contents: 
+${answers.toc} 
+# installation:
+${answers.installation}
+# Usage:
+${answers.usage}
+# License:
+${answers.license}
+# Contributing:
+${answers.contributing}
+# Test:
+${answers.test}
+# Questions:
+${answers.questions}
+
+`;
+
+
+    // Write the README file
+    fs.writeFile('README.md', readmeContent, (err) =>
+    err ? console.error(err) : console.log('README generated!')
+  );
+  }).then(()=>{
+
+  });
+
+
+
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
+
+// // const writeFileAsync = util.promisify(fs.writeFile);
+
+// const promptUser = () =>
+//   inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'description',
+//       message: 'Description?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'toc',
+//       message: 'Table of Contents?',
+//     },
+    
+//     {
+//       type: 'input',
+//       name: 'installation',
+//       message: 'Installation',
+//     },
+//     {
+//       type: 'input',
+//       name: 'usage',
+//       message: 'Usage',
+//     },
+//     {
+//       type: 'list',
+//       name: 'license',
+//       message: 'License?',
+//       choices: ['MIT', 'GPL', 'Apache 2.0']
+//     },
+//     {
+//       type: 'input',
+//       name: 'contributing',
+//       message: 'Contributing',
+//     },
+//     {
+//       type: 'input',
+//       name: 'tests',
+//       message: 'Tests',
+//     },
+//     {
+//       type: 'input',
+//       name: 'questions',
+//       message: 'Questions',
+//     },
+//   ]);
+
+// const generateHTML = (answers) =>
+
+
+
+
+
+
+
+
+
+
+
+// promptUser()
+//   .then((answers) => writeFileAsync('README.md', generateHTML(answers)))
+//   .then(() => console.log('Successfully wrote to README.md'))
+//   .catch((err) => console.error(err));
+
+//   fs.writeFile('README.md', readmeContents, err => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log('README.md file created!');
+//     }
+//   });
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4"> # ${answers.description}</h1>
+//     <p> ${answers.toc}</p>
+//     <h3> ## How to install?</h3>
+//     <p> * ${answers.installation}</p>
+//     <h3> ## Usage</h3>
+//     <p> * ${answers.usage}</p>
+//     <h3> ## License </h3>
+//     <p> ${answers.license} </p>
+//     <h3> ## Contributing: </h3>
+//     <p> * ${answers.contributing} </p>
+//     <h3> ## Tests: </h3>
+//     <p> * ${answers.tests} </p>
+//     <h3> ## Questions: </h3>
+//     <p> * ${answers.questions} </p>
+//     <p>![Kiku](put your image file name here)</p>
+//   </div>
+// </div>
+// </body>
+// </html>`;
